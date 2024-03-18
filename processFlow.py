d32744aff322212f4a103cf5ff5435dceaad5b42
ChiaraDeMartin95 <chiara.demartin.keira@gmail.com>
2024-03-18 17:04:30 +0100

    fix bug in pt intervals + compute number of signal and bkg candidates in pt intervals

--- processFlow.py
+++ processFlow.py
@@ -43,16 +43,15 @@ vars_to_draw = sigCandidates.get_var_names()
 leg_labels = ['background', 'signal']
 plot_utils.plot_distr([bkgCandidates, sigCandidates], vars_to_draw, bins=100, labels=leg_labels, log=True, density=True, figsize=(12, 7), alpha=0.3, grid=False)
 plt.subplots_adjust(left=0.06, bottom=0.06, right=0.99, top=0.96, hspace=0.55, wspace=0.55)
-plt.savefig("Distributions.png")
+plt.savefig("TrainingPlots/Distributions.png")
 
 vars_to_draw_mass = ['fMassXi']
 if not isXi: 
     vars_to_draw_mass = ['fMassOmega']
 plot_utils.plot_distr([bkgCandidates, sigCandidates], vars_to_draw_mass, bins=100, labels=leg_labels, log=False, density=True, figsize=(12, 7), alpha=0.3, grid=False)
 plt.subplots_adjust(left=0.06, bottom=0.06, right=0.99, top=0.96, hspace=0.55, wspace=0.55)
-plt.savefig("MassXi.png")
+plt.savefig("TrainingPlots/MassXi.png")
 
-#'fPt','fMassXi', 'fMassOmega',
 vars_to_draw_bis = [
                 'fCascRadius', 'fV0Radius', 'fCascCosPA', 
                 'fV0CosPA', 'fDCAPosToPV', 'fDCANegToPV', 
@@ -60,30 +59,39 @@ vars_to_draw_bis = [
                 'fDCAV0ToPV','fBachBaryonCosPA', 'fBachBaryonDCAxyToPV']
 plot_utils.plot_distr([bkgCandidates, sigCandidates], vars_to_draw_bis, bins=100, labels=leg_labels, log=True, density=True, figsize=(12, 7), alpha=0.3, grid=False)
 plt.subplots_adjust(left=0.06, bottom=0.06, right=0.99, top=0.96, hspace=0.55, wspace=0.55)
-plt.savefig("DistributionsInputOnly.png")
+plt.savefig("TrainingPlots/DistributionsInputOnly.png")
 
 plot_utils.plot_corr([bkgCandidates, sigCandidates], vars_to_draw, leg_labels)
-plt.savefig("Correlations.png")
+plt.savefig("TrainingPlots/Correlations.png")
 #plt.show()
 
 npt = 5
 minpt = 0.6
 if not isXi: 
     minpt = 0.8
-ptbin = [minpt, 1, 2, 3, 4, 5, 10]
-
+ptbin = [minpt, 1.0, 2.0, 3.0, 4.0, 5.0, 10.0]
+x = slice(1, 7)
+ptbinMax = ptbin[x] 
+print('ptbinMax: ', ptbinMax)
 if isXi:
     Cascade_string = 'Xi'
 else:
     Cascade_string = 'Omega'
 
-for pt in ptbin:
-    if pt == 10: break
-    print('pt bin: ', pt)
-    preselection_string_pt = 'fPt > ' + str(pt) + ' and fPt < ' + str(pt+1)
+nsig = [0, 0, 0, 0, 0, 0, 0]
+nbkg = [0, 0, 0, 0, 0, 0, 0]
+
+for ptbin, ptbinMax, nsig, nbkg in zip(ptbin, ptbinMax, nsig, nbkg):
+    if ptbin == 1: break
+    print('pt bin: ', ptbin)
+    print('pt+1 bin: ', ptbinMax)
+    preselection_string_pt = 'fPt > ' + str(ptbin) + ' and fPt < ' + str(ptbinMax)
     sigCandidatesNew = sigCandidates.apply_preselections(preselection_string_pt, False)
     bkgCandidatesNew = bkgCandidates.apply_preselections(preselection_string_pt, False)
-
+    nsig = sigCandidatesNew.get_n_cand()
+    nbkg = bkgCandidatesNew.get_n_cand()
+    print('nsig: ', nsig)
+    print('nbkg: ', nbkg)
     train_test_data = train_test_generator([sigCandidatesNew, bkgCandidatesNew], [1,0], test_size=0.5, random_state=42)
 
     features_for_train = vars_to_draw.copy()
@@ -97,6 +105,18 @@ for pt in ptbin:
     features_for_train.remove('fEta')
     features_for_train.remove('fMassLambdaDau')
     features_for_train.remove('fMultFT0M')
+
+    #Draw input features in pt intervals
+    plot_utils.plot_distr([bkgCandidatesNew, sigCandidatesNew], vars_to_draw_bis, bins=100, labels=leg_labels, log=True, density=True, figsize=(12, 7), alpha=0.3, grid=False)
+    plt.subplots_adjust(left=0.06, bottom=0.06, right=0.99, top=0.96, hspace=0.55, wspace=0.55)
+    plt.savefig("TrainingPlots/DistributionsInputOnly_" + str(ptbin)+ ".png")
+
+    #Draw all features in pt intervals
+    vars_to_draw = sigCandidates.get_var_names()
+    leg_labels = ['background', 'signal']
+    plot_utils.plot_distr([bkgCandidatesNew, sigCandidatesNew], vars_to_draw, bins=100, labels=leg_labels, log=True, density=True, figsize=(12, 7), alpha=0.3, grid=False)
+    plt.subplots_adjust(left=0.06, bottom=0.06, right=0.99, top=0.96, hspace=0.55, wspace=0.55)
+    plt.savefig("TrainingPlots/Distributions_" + str(ptbin)+ ".png")
     
     model_clf = xgb.XGBClassifier()
     model_hdl = ModelHandler(model_clf, features_for_train)
@@ -104,8 +124,8 @@ for pt in ptbin:
     #optimization of the parameters
     hyper_pars_ranges = {'n_estimators': (200, 1000), 'max_depth': (
         2, 4), 'learning_rate': (0.01, 0.1)}
-    model_hdl.optimize_params_optuna(train_test_data, hyper_pars_ranges, cross_val_scoring='roc_auc', timeout=120,
-                                    # n_jobs=-1, n_trials=100, direction='maximize')
+    #model_hdl.optimize_params_optuna(train_test_data, hyper_pars_ranges, cross_val_scoring='roc_auc', timeout=120,
+     #                                n_jobs=-1, n_trials=100, direction='maximize')
     
     #training of the model
     model_hdl.train_test_model(train_test_data)
@@ -118,17 +138,17 @@ for pt in ptbin:
     ml_out_fig = plot_utils.plot_output_train_test(model_hdl, train_test_data, 100, 
                                                    False, leg_labels, True, density=True)
     
-    plt.savefig("BDTScore" + str(pt) +".png")
+    plt.savefig("TrainingPlots/BDTScore" + str(ptbin) +".png")
     
     #ROC curve
     roc_train_test_fig = plot_utils.plot_roc_train_test(train_test_data[3], y_pred_test,
                                                         train_test_data[1], y_pred_train, None, leg_labels)
     
-    plt.savefig("ROC" + str(pt) +".png")
+    plt.savefig("TrainingPlots/ROC" + str(ptbin) +".png")
     
     #feature importance
     plot_utils.plot_feature_imp(train_test_data[2], train_test_data[3], model_hdl) 
-    plt.savefig("FeatureImportance" + str(pt) +".png")
+    plt.savefig("TrainingPlots/FeatureImportance" + str(ptbin) +".png")
     
     #dataCandidates.apply_model_handler(model_hdl, False)
     #selected_data_hndl = dataCandidates.get_subset('model_output>0.7')
@@ -144,10 +164,14 @@ for pt in ptbin:
     #plt.savefig("InvMassBkgXi" + str(pt) +".png")
     
     #store the trained model
-    model_hdl.dump_model_handler("./ModelHandler_" + str(pt) +".pickle")
-    model_hdl.dump_original_model("./XGBoostModel_" + str(pt) +".pickle")
+    model_hdl.dump_model_handler("./ModelHandler_" + str(ptbin) +".pickle")
+    model_hdl.dump_original_model("./XGBoostModel_" + str(ptbin) +".pickle")
     
     #convert model in ONNX
     model_converter = H4MLConverter(model_hdl) # create the converter object
     model_onnx = model_converter.convert_model_onnx(1, len(features_for_train))
-    model_converter.dump_model_onnx("model_onnx"+ Cascade_string + "_" + str(pt) +".onnx") # dump the model in ONNX format
+    model_converter.dump_model_onnx("Onnx/model_onnx"+ Cascade_string + "_" + str(ptbin) +".onnx") # dump the model in ONNX format
+
+plt.title("Number of signal candidates")
+plt.plot(ptbin, nsig, color="red")
+plt.savefig("TrainingPlots/NumberSignal.png")
