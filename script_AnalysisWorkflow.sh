4dee3cd52999ea211ab14f89531d37bcc5b2aa92
ChiaraDeMartin95 <chiara.demartin.keira@gmail.com>
2025-10-30 10:41:19 +0100

    update

--- script_AnalysisWorkflow.sh
+++ script_AnalysisWorkflow.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
-root -l -b -q "ProcessTree.C()" #produces histograms from tree
-root -l -b -q "ComputeV2.C()" #from 3D histograms produces 1D histograms of mass and V2 
+#root -l -b -q "ProcessTree.C()" #produces histograms from tree
+#root -l -b -q "ComputeV2.C()" #from 3D histograms produces 1D histograms of mass and V2 
 #MultType = 1 for FT0M, isMB = 0, mul = i
-for i in `seq 0 7`
+for i in `seq 0 8`
     do
-    root -l -b -q "FitV2.C($i)" #fit of inv mass and V2 histograms    
+    #root -l -b -q "FitV2orPol.C(1,1,0,$i)"
+    #root -l -b -q "FitV2orPol.C(1,0,0,$i)"
+    root -l -b -q "FitV2orPol.C(1,1,0,$i)"
+    #root -l -b -q "FitV2orPol.C(0,1,0,$i,2)" #XiMinus
+    #root -l -b -q "FitV2orPol.C(0,0,0,$i,2)" #XiMinus
+    #root -l -b -q "FitV2orPol.C(1,1,0,$i,3)" #XiPlus
+    #root -l -b -q "FitV2orPol.C(1,0,0,$i,3)" #XiPlus
+    #root -l -b -q "FitV2orPol.C(1,0,0,$i,3)" #XiPlus
+    #root -l -b -q "FitV2orPol.C(1,1,0,$i,2)" #XiMinus
+    #root -l -b -q "FitV2orPol.C(1,1,0,$i,3)" #XiPlus
+    #root -l -b -q "FitV2orPol.C(0,0,0,$i,2)" #XiMinus
+    #root -l -b -q "FitV2orPol.C(0,0,0,$i,3)" #XiPlus
+    #root -l -b -q "FitV2orPol.C(0,1,0,$i,2)" #XiMinus
+    #root -l -b -q "FitV2orPol.C(0,1,0,$i,3)" #XiPlus
+    #root -l -b -q "FitV2orPol.C(1,0,0,$i,0)" #Xi v2
     done
+
+#pt analysis, polfromLambda
