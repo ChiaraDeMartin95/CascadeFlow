bfacb16297c5a9d89153c393db5c547911dabbfc
ChiaraDeMartin95 <chiara.demartin.keira@gmail.com>
2025-10-30 10:40:51 +0100

    update

--- script_MultiTrial.sh
+++ script_MultiTrial.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
-
-#for j in 0
-for j in `seq 1 8` #number of trials
-do
-    root -l -b -q "ProcessTree.C($j)" #produces histograms from tree
+#for i in `seq 4 8` #centrality bins
+#for i in `seq 3 3`
+ #do
+ for j in `seq 0 19` #BDT
+  do
+  #for l in `seq 0 8` #mass
+    #do
+    root -l -b -q "ProcessTHN.C($j)" #produces histograms from tree
     root -l -b -q "ComputeV2.C($j)" #from 3D histograms produces 1D histograms of mass and V2 
-
-    for i in `seq 0 7` #centrality bins
-    do
-    root -l -b -q "FitV2.C($j, $i)" #fit of inv mass and V2 histograms    
-    done
-done
+    #root -l -b -q "ComputeEff.C(0, $j)"    
+    #root -l -b -q "FitV2orPol.C(1, 0, 0, $i, $j)" #fit of inv mass and V2 histograms (j is for the mass cut)
+    #root -l -b -q "FitV2orPol.C(1, 0, 0, $i)" #fit of inv mass and V2 histograms (j is for the mass cut)
+    #root -l -b -q "FitV2orPol.C(1, 1, $j, $i, $l)" #fit of inv mass and V2 histograms (j is for the mass cut)   
+    #root -l -b -q "FitV2orPol.C(1, 0, 0, $i, 0, 1, 6)" #for Lambdas
+    #root -l -b -q "MultiTrial.C($i, 2)" #fit of inv mass and V2 histograms    
+    #echo "hello"
+    #for j in `seq 0 21` #number of trials
+    #do
+    #for l in `seq 0 6` #number of mass interval trials
+    #do
+    #root -l -b -q "FitV2orPol.C(1, 1, $j, $i, $l)" #fit of inv mass and V2 histograms      
+    #done
+   done
+#done
