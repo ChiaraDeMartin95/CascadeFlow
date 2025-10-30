82eb1870d258d941a33c137fc956c0c01770e333
ChiaraDeMartin95 <chiara.demartin.keira@gmail.com>
2025-10-30 10:39:15 +0100

    add resolution for V0A

--- Resolution.C
+++ Resolution.C
@@ -124,17 +124,23 @@ void Resolution(Bool_t isSPReso = 1, Bool_t isLFReso = 1)
   {
     cout << "You are looking for file: " << ComputeResoFileName << endl;
     cout << "Error: CFW resolution not available" << endl;
-    return;
+    // return;
   }
   TFile *inputfile = new TFile(ComputeResoFileName, "");
   TString nameQT0CTPCA = "QVectorsT0CTPCA";
   TString nameQT0CTPCC = "QVectorsT0CTPCC";
   TString nameQTPCAC = "QVectorsTPCAC";
+  TString nameQT0CV0A = "QVectorsT0CV0A";
+  TString nameQV0ATPCA = "QVectorsV0ATPCA";
+  TString nameQV0ATPCC = "QVectorsV0ATPCC";
   if (!isSPReso)
   {
     nameQT0CTPCA = "QVectorsNormT0CTPCA";
     nameQT0CTPCC = "QVectorsNormT0CTPCC";
     nameQTPCAC = "QVectorsNormTPCAC";
+    nameQT0CV0A = "QVectorsNormT0CV0A";
+    nameQV0ATPCA = "QVectorsNormV0ATPCA";
+    nameQV0ATPCC = "QVectorsNormV0ATPCC";
   }
 
   cout << "InputFile: " << ComputeResoFileName << endl;
@@ -144,6 +150,7 @@ void Resolution(Bool_t isSPReso = 1, Bool_t isLFReso = 1)
     return;
   }
   TDirectory *dir = (TDirectory *)inputfile->Get("lf-cascade-flow/resolution");
+  // TDirectory *dir = (TDirectory *)inputfile->Get("lf-cascade-flow");
   if (!dir)
   {
     cout << "Error: input directory not found" << endl;
@@ -152,47 +159,98 @@ void Resolution(Bool_t isSPReso = 1, Bool_t isLFReso = 1)
   TH2F *hQT0CTPCA = (TH2F *)dir->Get(nameQT0CTPCA);
   TH2F *hQT0CTPCC = (TH2F *)dir->Get(nameQT0CTPCC);
   TH2F *hQTPCAC = (TH2F *)dir->Get(nameQTPCAC);
+  TH2F *hQT0CV0A = (TH2F *)dir->Get(nameQT0CV0A);
+  TH2F *hQV0ATPCA = (TH2F *)dir->Get(nameQV0ATPCA);
+  TH2F *hQV0ATPCC = (TH2F *)dir->Get(nameQV0ATPCC);
   if (!hQT0CTPCA || !hQT0CTPCC || !hQTPCAC)
   {
     cout << "Error: input histograms not found" << endl;
     return;
   }
+  if (!hQT0CV0A)
+  {
+    hQT0CV0A = (TH2F *)dir->Get(nameQT0CTPCA);
+    cout << "Error: input histograms T0CV0A not found, taking T0CTPCA instead" << endl;
+  }
+  if (!hQV0ATPCA)
+  {
+    hQV0ATPCA = (TH2F *)dir->Get(nameQT0CTPCC);
+    cout << "Error: input histograms V0ATPCA not found, taking T0CTPCC instead" << endl;
+  }
+  if (!hQV0ATPCC)
+  {
+    hQV0ATPCC = (TH2F *)dir->Get(nameQTPCAC);
+    cout << "Error: input histograms V0ATPCC not found, taking TPCAC instead" << endl;
+  }
 
   TH1D *hReso = new TH1D("hReso", "hReso", numCent, fCentFT0C);
+  if (isOOCentrality)
+    hReso = new TH1D("hReso", "hReso", numCentLambdaOO, fCentFT0CLambdaOO);
+  TH1D *hResoV0A = (TH1D*)hReso->Clone("hResoV0A");  
+  TH1D *hResoPerCentBins = new TH1D("hResoPerCentBins", "hResoPerCentBins", 100, 0, 100);
+  TH1D *hResoPerCentBinsV0A = new TH1D("hResoPerCentBinsV0A", "hResoPerCentBinsV0A", 100, 0, 100);
   TH1D *hReso080 = new TH1D("hReso080", "hReso080", 1, 0, 1);
   Int_t CentFT0CMax = 0;
   Int_t CentFT0CMin = 0;
-  for (Int_t cent = 0; cent < numCent + 1; cent++)
+  Int_t commonnumCent = numCent;
+  if (isOOCentrality)
+    commonnumCent = numCentLambdaOO;
+  for (Int_t cent = 0; cent < commonnumCent + 1; cent++)
   {
-    
-    if (cent == numCent)
+
+    if (cent == commonnumCent)
     { // 0-80%
       CentFT0CMin = 0;
       CentFT0CMax = 80;
+      if (isOOCentrality)
+      {
+        CentFT0CMin = 0;
+        CentFT0CMax = 90;
+      }
     }
     else
     {
       CentFT0CMin = CentFT0C[cent];
       CentFT0CMax = CentFT0C[cent + 1];
+      if (isOOCentrality)
+      {
+        CentFT0CMin = CentFT0CLambdaOO[cent];
+        CentFT0CMax = CentFT0CLambdaOO[cent + 1];
+      }
     }
     cout << "Centrality: " << CentFT0CMin << "-" << CentFT0CMax << "%" << endl;
     hQT0CTPCA->GetYaxis()->SetRangeUser(CentFT0CMin + 0.001, CentFT0CMax - 0.001);
     hQT0CTPCC->GetYaxis()->SetRangeUser(CentFT0CMin + 0.001, CentFT0CMax - 0.001);
     hQTPCAC->GetYaxis()->SetRangeUser(CentFT0CMin + 0.001, CentFT0CMax - 0.001);
+    hQT0CV0A->GetYaxis()->SetRangeUser(CentFT0CMin + 0.001, CentFT0CMax - 0.001);
+    hQV0ATPCA->GetYaxis()->SetRangeUser(CentFT0CMin + 0.001, CentFT0CMax - 0.001);
+    hQV0ATPCC->GetYaxis()->SetRangeUser(CentFT0CMin + 0.001, CentFT0CMax - 0.001);
     TH1F *h1QT0CTPCA = (TH1F *)hQT0CTPCA->ProjectionX("h1QT0CTPCA");
     TH1F *h1QT0CTPCC = (TH1F *)hQT0CTPCC->ProjectionX("h1QT0CTPCC");
     TH1F *h1QTPCAC = (TH1F *)hQTPCAC->ProjectionX("h1QTPCAC");
+    TH1F *h1QT0CV0A = (TH1F *)hQT0CV0A->ProjectionX("h1QT0CV0A");
+    TH1F *h1QV0ATPCA = (TH1F *)hQV0ATPCA->ProjectionX("h1QV0ATPCA");
+    TH1F *h1QV0ATPCC = (TH1F *)hQV0ATPCC->ProjectionX("h1QV0ATPCC");
     Float_t MeanT0CTPCA = h1QT0CTPCA->GetMean();
     Float_t MeanT0CTPCC = h1QT0CTPCC->GetMean();
     Float_t MeanTPCAC = h1QTPCAC->GetMean();
+    Float_t MeanT0CV0A = h1QT0CV0A->GetMean();
+    Float_t MeanV0ATPCA = h1QV0ATPCA->GetMean();
+    Float_t MeanV0ATPCC = h1QV0ATPCC->GetMean();
     Float_t ErrMeanT0CTPCA = h1QT0CTPCA->GetMeanError();
     Float_t ErrMeanT0CTPCC = h1QT0CTPCC->GetMeanError();
     Float_t ErrMeanTPCAC = h1QTPCAC->GetMeanError();
+    Float_t ErrMeanT0CV0A = h1QT0CV0A->GetMeanError();
+    Float_t ErrMeanV0ATPCA = h1QV0ATPCA->GetMeanError();
+    Float_t ErrMeanV0ATPCC = h1QV0ATPCC->GetMeanError();
     cout << "MeanT0CTPCA: " << MeanT0CTPCA << " MeanT0CTPCC: " << MeanT0CTPCC << " MeanTPCAC: " << MeanTPCAC << " Reso: " << sqrt(MeanT0CTPCA * MeanT0CTPCC / MeanTPCAC) << endl;
+    cout << "MeanT0CV0A: " << MeanT0CV0A << " MeanV0ATPCA: " << MeanV0ATPCA << " MeanV0ATPCC: " << MeanV0ATPCC << "Reso: " << sqrt(MeanT0CV0A * MeanV0ATPCA / MeanV0ATPCA) << endl;
     // cout << "Sourav resolution (EP): " << ftcReso[cent] << endl;
     Float_t RelErr2 = pow(ErrMeanT0CTPCA / MeanT0CTPCA, 2) + pow(ErrMeanT0CTPCC / MeanT0CTPCC, 2) + pow(ErrMeanTPCAC / MeanTPCAC, 2);
     Float_t ErrReso = sqrt(RelErr2 * (MeanT0CTPCA * MeanT0CTPCC / MeanTPCAC));
-    if (cent == numCent)
+    Float_t RelErr2V0A = pow(ErrMeanV0ATPCA / MeanV0ATPCA, 2) + pow(ErrMeanT0CTPCA / MeanT0CTPCA, 2) + pow(ErrMeanT0CV0A / MeanT0CV0A, 2);
+    Float_t ErrResoV0A = sqrt(RelErr2V0A * (MeanT0CV0A * MeanT0CTPCA / MeanV0ATPCA));
+    if (cent == commonnumCent)
     {
       hReso080->SetBinContent(1, sqrt(MeanT0CTPCA * MeanT0CTPCC / MeanTPCAC));
       hReso080->SetBinError(1, ErrReso);
@@ -201,13 +259,48 @@ void Resolution(Bool_t isSPReso = 1, Bool_t isLFReso = 1)
     {
       hReso->SetBinContent(cent + 1, sqrt(MeanT0CTPCA * MeanT0CTPCC / MeanTPCAC));
       hReso->SetBinError(cent + 1, ErrReso);
+      hResoV0A->SetBinContent(cent + 1, sqrt(MeanT0CV0A * MeanT0CTPCA / MeanTPCAC));
+      hResoV0A->SetBinError(cent + 1, ErrResoV0A);
     }
+  }
+  // per cent bins
+  for (Int_t cent = 0; cent < 100; cent++)
+  {
+    hQT0CTPCA->GetYaxis()->SetRangeUser(cent + 0.001, cent + 0.001);
+    hQT0CTPCC->GetYaxis()->SetRangeUser(cent + 0.001, cent + 0.001);
+    hQTPCAC->GetYaxis()->SetRangeUser(cent + 0.001, cent + 0.001);
+    hQT0CV0A->GetYaxis()->SetRangeUser(cent + 0.001, cent + 0.001);
+    TH1F *h1QT0CTPCA = (TH1F *)hQT0CTPCA->ProjectionX("h1QT0CTPCA");
+    TH1F *h1QT0CTPCC = (TH1F *)hQT0CTPCC->ProjectionX("h1QT0CTPCC");
+    TH1F *h1QTPCAC = (TH1F *)hQTPCAC->ProjectionX("h1QTPCAC");
+    TH1F *h1QT0CV0A = (TH1F *)hQT0CV0A->ProjectionX("h1QT0CV0A");
+    Float_t MeanT0CTPCA = h1QT0CTPCA->GetMean();
+    Float_t MeanT0CTPCC = h1QT0CTPCC->GetMean();
+    Float_t MeanTPCAC = h1QTPCAC->GetMean();
+    Float_t MeanT0CV0A = h1QT0CV0A->GetMean();
+    Float_t MeanV0ATPC = h1QT0CV0A->GetMean();
+    Float_t ErrMeanT0CTPCA = h1QT0CTPCA->GetMeanError();
+    Float_t ErrMeanT0CTPCC = h1QT0CTPCC->GetMeanError();
+    Float_t ErrMeanTPCAC = h1QTPCAC->GetMeanError();
+    Float_t ErrMeanT0CV0A = h1QT0CV0A->GetMeanError();
+    Float_t ErrMeanV0ATPC = h1QT0CV0A->GetMeanError();
+    Float_t RelErr2 = pow(ErrMeanT0CTPCA / MeanT0CTPCA, 2) + pow(ErrMeanT0CTPCC / MeanT0CTPCC, 2) + pow(ErrMeanTPCAC / MeanTPCAC, 2);
+    Float_t ErrReso = sqrt(RelErr2 * (MeanT0CTPCA * MeanT0CTPCC / MeanTPCAC));
+    Float_t ErrResoV0A = sqrt(RelErr2 * (MeanT0CV0A * MeanT0CTPCC / MeanTPCAC));
+    hResoPerCentBins->SetBinContent(cent + 1, sqrt(MeanT0CTPCA * MeanT0CTPCC / MeanTPCAC));
+    hResoPerCentBinsV0A->SetBinContent(cent + 1, sqrt(MeanV0ATPC * MeanT0CTPCC / MeanTPCAC));
+    cout << "Cent: " << cent << " Reso: " << sqrt(MeanT0CTPCA * MeanT0CTPCC / MeanTPCAC) << endl;
+    cout << MeanT0CTPCA << " " << MeanT0CTPCC << " " << MeanTPCAC << " " << ErrMeanT0CTPCA << " " << ErrMeanT0CTPCC << " " << ErrMeanTPCAC << endl;
+    hResoPerCentBins->SetBinError(cent + 1, ErrReso);
+    hResoPerCentBinsV0A->SetBinError(cent + 1, ErrResoV0A);
   }
 
   gStyle->SetOptStat(0);
   TCanvas *canvas = new TCanvas("canvas", "canvas", 800, 600);
   StyleCanvas(canvas, 0.05, 0.1, 0.1, 0.05);
   hReso->GetYaxis()->SetRangeUser(0., 1.5);
+  if (isOOCentrality)
+    hReso->GetYaxis()->SetRangeUser(0., 0.65);
   hReso->SetLineColor(kBlack);
   hReso->SetMarkerColor(kBlack);
   hReso->SetMarkerStyle(20);
@@ -222,14 +315,29 @@ void Resolution(Bool_t isSPReso = 1, Bool_t isLFReso = 1)
   hReso->GetYaxis()->SetTitleOffset(1.);
   hReso->SetTitle("");
   hReso->Draw("");
+  hResoV0A->SetLineColor(kGreen + 2);
+  hResoV0A->SetMarkerColor(kGreen + 2);
+  hResoV0A->SetMarkerStyle(23);
+  hResoV0A->SetMarkerSize(1.5);
+  hResoV0A->Draw("same");
+  hResoPerCentBins->SetLineColor(kBlue);
+  hResoPerCentBins->SetMarkerColor(kBlue);
+  hResoPerCentBins->SetMarkerStyle(21);
+  hResoPerCentBins->SetMarkerSize(0.8);
+  //hResoPerCentBins->Draw("same");
+  hResoPerCentBinsV0A->SetLineColor(kRed);
+  hResoPerCentBinsV0A->SetMarkerColor(kRed);
+  hResoPerCentBinsV0A->SetMarkerStyle(22);
+  hResoPerCentBinsV0A->SetMarkerSize(0.8);
+  hResoPerCentBinsV0A->Draw("same");
 
   TH1F *hResoSourav = (TH1F *)hReso->Clone("hResoSourav");
   hResoSourav->SetLineColor(kRed);
   hResoSourav->SetMarkerColor(kRed);
-  for (Int_t cent = 0; cent < numCent; cent++)
+  for (Int_t cent = 0; cent < commonnumCent; cent++)
   {
-    hResoSourav->SetBinContent(cent + 1, ftcResoSourav[cent]);
-    hResoSourav->SetBinError(cent + 1, 0);
+    // hResoSourav->SetBinContent(cent + 1, ftcResoSourav[cent]);
+    // hResoSourav->SetBinError(cent + 1, 0);
   }
   // if (!isSPReso)
   // hResoSourav->Draw("same");
@@ -265,6 +373,7 @@ void Resolution(Bool_t isSPReso = 1, Bool_t isLFReso = 1)
   TFile *outputfile = new TFile(Soutputfile + ".root", "RECREATE");
   hReso->Write();
   hReso080->Write();
+  hResoPerCentBins->Write();
   outputfile->Close();
   cout << "\nOutputFile: " << Soutputfile << endl;
 }
