0d22441b3eecedd0164316dd28cf6bf0b37f7dcf
ChiaraDeMartin95 <chiara.demartin.keira@gmail.com>
2026-01-30 11:07:11 +0100

    Add measured V2 in PbPb and fix dndeta in OO with preliminary results

--- CommonVarLambda.h
+++ CommonVarLambda.h
@@ -35,7 +35,7 @@ TString IsOneOrTwoGauss[2] = {"_OneGaussFit", ""};
 TString SIsBkgParab[4] = {"_BkgRetta", "_BkgParab", "_BkgPol3", "_BkgExpo"};
 Float_t ParticleMassPDG[numPart] = {1.32171, 1.67245, 1.32171, 1.32171, 1.67245, 1.67245, 1.115683, 1.115683, 1.115683}; // Xi+-, Omega+-, Xi-, Xi+, Omega-, Omega+, Lambda + ALambda
 TString ParticleName[numPart] = {"Xi", "Omega", "XiMinus", "XiPlus", "OmegaMinus", "OmegaPlus", "Lambda", "LambdaPart", "AntiLambda"};
-TString ParticleNameLegend[numPart] = {"#Xi^{#pm}", "#Omega^{#pm}", "#Xi^{-}", "#Xi^{+}", "#Omega^{-}", "#Omega^{+}", "#Lambda + #bar{#Lambda}", "#Lambda", "#bar{#Lambda}"};
+TString ParticleNameLegend[numPart] = {"#Xi^{#pm}", "#Omega^{#pm}", "#Xi^{-}", "#Xi^{+}", "#Omega^{-}", "#Omega^{+}", "#Lambda + #overline{#Lambda}", "#Lambda", "#overline{#Lambda}"};
 TString TypeHisto[numChoice] = {"Mean", "SigmaWeighted", "Purity", "Yield", "V2Mixed", "Pzs2Mixed", "Pzs2LambdaFromCMixed", "Cos2ThetaNoFit", "Cos2ThetaLambdaFromC", "V2MixedCorr", "Cos2ThetaLambdaFromCVsPt", "Cos2ThetaLambdaFromCVsEta"};
 TString TitleY[numChoice] = {"Mean (GeV/#it{c}^{2})", "Sigma (GeV/#it{c}^{2})", "S/(S+B)", "1/#it{N}_{evt} d#it{N}/d#it{p}_{T} (GeV/#it{c})^{-1}", "v2", "Pz,s2", "Pz,s2", "#LTcos^{2}(#theta*_{#Lambda})#GT", "#LTcos^{2}(#theta*_{p})#GT", "v2, corr", "#LTcos^{2}(#theta*_{p})#GT", "#LTcos^{2}(#theta*_{p})#GT"};
 TString TitleXPt = "#it{p}_{T} (GeV/#it{c})";
@@ -47,12 +47,14 @@ Int_t CentFT0C[numCent + 1] = {0, 10, 20, 30, 40, 50, 60, 70, 80}; //{0, 30, 50,
 Double_t fCentFT0C[numCent + 1] = {0, 10, 20, 30, 40, 50, 60, 70, 80};
 Int_t CentFT0CLambdaOO[numCentLambdaOO + 1] = {0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
 Double_t fCentFT0CLambdaOO[numCentLambdaOO + 1] = {0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
-Double_t dNdEtaAbhi[numCent] = {(2080. + 1697.) / 2, 1274, 862, 566, 355, 208, 112, 54}; // values from Abhi
-Double_t dNdEtaAbhiErr[numCent] = {63, 40, 27, 19, 13, 8, 5, 3};
-Double_t dNdEtaOOPrel[numCentLambdaOO] = {(126.95 + 104.16) / 2, 84.30, 63.98, 48.26, 35.99, 26.43, 19.03, 13.22, 8.50}; // approved up to 60%
-Double_t dNdEtaOOErrPrel[numCentLambdaOO] = {(4.23 + 3.44) / 2, 2.76, 2.08, 1.55, 1.13, 0.81, 0.57, 0.39, 0.23};         // approved up to 60%
-Double_t dNdEtaOO[numCentLambdaOO] = {(126.95 + 104.16) / 2, 84.30, 63.98, 48.26, 35.99, 26.43, 19.91, 14.87, 11.11};    // from 60% to 90%, extrapolated with MultVsCent.C macro
-Double_t dNdEtaOOErr[numCentLambdaOO] = {(4.23 + 3.44) / 2, 2.76, 2.08, 1.55, 1.13, 0.81, 0.58, 0.54, 0.48};             // from 60% to 90%, extrapolated with MultVsCent.C macro
+Double_t dNdEtaAbhi[numCent] = {(2047. + 1668.) / 2, 1253, 848, 559, 351, 205, 110, 53}; // values from Abhi
+Double_t dNdEtaAbhiErr[numCent] = {(54. + 42.)/2, 33, 25, 19, 14, 11, 8, 5};
+Double_t dNdEtaOOPrel[numCentLambdaOO] = {(126.6660 + 106.8340) / 2, 87.2877, 67.1562, 51.1201, 37.8919, 26.9060, 19.03, 13.22, 8.50, 0}; // approved up to 60%
+Double_t dNdEtaOOErrPrel[numCentLambdaOO] = {(4.23 + 3.44) / 2, 2.76, 2.08, 1.55, 1.13, 0.81, 0.57, 0.39, 0.23, 0};         // approved up to 60%
+Double_t dNdEtaOOErrPrelSyst[numCentLambdaOO] = {(4.1009 + 3.3921) / 2, 2.8101, 2.2507, 1.8847, 1.6370, 1.4909, 0.57, 0.39, 0.23, 0};         // approved up to 60%
+Double_t dNdEtaOO[numCentLambdaOO] = {(126.6660 + 106.8340) / 2, 87.2877, 67.1562, 51.1201, 37.8919, 26.9060, 19.91, 14.87, 11.11, 0};    // from 60% to 90%, extrapolated with MultVsCent.C macro
+Double_t dNdEtaOOErr[numCentLambdaOO] = {(0.0335 + 0.0250) / 2, 0.0143, 0.0117, 0.0099, 0.0084, 0.0069, 0.58, 0.54, 0.48, 0};             // from 60% to 90%, extrapolated with MultVsCent.C macro
+Double_t dNdEtaOOErrSyst[numCentLambdaOO] = {(4.1009 + 3.3921) / 2, 2.8101, 2.2507, 1.8847, 1.6370, 1.4909, 0.58, 0.54, 0.48, 0};             // from 60% to 90%, extrapolated with MultVsCent.C macro
 Double_t dNdEtaNeNe[2] = {105.59, 20.63};                                                                                // for Junlee results. Averages computed from analysis note (0-40%, 40-90% even if polarization uses 40-100%; multiplicity available only up to 90%)
 Double_t dNdEtaNeNeErr[2] = {3.52, 0.69};                                                                                // random reasonable errors assigned
 
@@ -130,15 +132,22 @@ Float_t AlphaLambdaErrors[numPart] = {1, 1, 0.008, 0.005, 0.008, 0.005, 1, 1, 1}
 // TString SinputFileName = "LHC25_OO_pass2_Train575744"; //THN
 // TString SinputFileName = "LHC25_OO_pass2_Train576496"; //tree with |eta| < 0.8 and |z| < 8 cm
 // TString SinputFileName = "LHC25_OO_pass2_Train589559"; //THN larger range pzs2
-TString SinputFileName = "LHC25_OO_pass2_Train589711"; // tree with new acceptance
+//TString SinputFileName = "LHC25_OO_pass2_Train589711"; // tree with new acceptance
+//TString SinputFileName = "LHC25_OO_pass2_Train589711Bis"; // tree with new acceptance
 //TString SinputFileName = "LHC25_OO_pass2_Train597528_NewAcc"; // THN with new acceptance (wrt previou: |etaDau| < 0.8)
 //TString SinputFileName = "LHC25_OO_pass2_Train597527_NewAcc_EtaPos"; // THN with new acceptance (wrt previou: |etaDau| < 0.8)
 //TString SinputFileName = "LHC25_OO_pass2_Train597526_NewAcc_EtaNeg"; // THN with new acceptance (wrt previou: |etaDau| < 0.8)
+TString SinputFileName = "LHC25_OO_pass2_Train598890"; 
+//TString SinputFileName = "LHC25_OO_pass2_Train598891_EtaPos"; 
+//TString SinputFileName = "LHC25_OO_pass2_Train598892_EtaNeg"; 
 
 // TString SinputFileNameAR = "LHC25_OO_pass2_Train510678";
 // TString SinputFileNameAR = "LHC25_OO_pass2_Train562132_wTHN";
 // TString SinputFileNameAR = "LHC25_OO_pass2_Train562850";
-TString SinputFileNameAR = "LHC25_OO_pass2_Train589711";
+//TString SinputFileNameAR = "LHC25_OO_pass2_Train589711";
+TString SinputFileNameAR = "LHC25_OO_pass2_Train598890";
+//TString SinputFileNameAR = "LHC25_OO_pass2_Train598891_EtaPos";
+//TString SinputFileNameAR = "LHC25_OO_pass2_Train598892_EtaNeg";
 // TString SinputFileNameAR = "LHC25_OO_pass2_Train567017"; //latest resolution
 
 // TString SinputFileNameCentWeight = "LHC25_OO_pass2_Train503805";
@@ -169,7 +178,8 @@ TString SinputFileNameResoWeight = "Resolution_EP_CFW_LHC25_OO_pass2_Train567017
 // TString SinputFileNameSyst = "LHC25_OO_pass2_Train503805";
 // TString SinputFileNameSyst = "LHC25_OO_pass2_Train510678_CorrectReso";
 // TString SinputFileNameSyst = "LHC23_PbPb_pass5_Train463979_ProtAcceptanceFromSecondayLambdas";
-// TString SinputFileNameSyst = "LHC25_OO_pass2_Train562850";
+//TString SinputFileNameSyst = "LHC25_OO_pass2_Train562850";
+//TString SinputFileNameSyst = "LHC25_OO_pass2_Train589711";
 TString SinputFileNameSyst = "LHC25_OO_pass2_Train589711";
 
 // File name for efficiency correction (if ExtrisApplyEffWeights == 1)
@@ -186,7 +196,7 @@ const float bdtCut[numCent + 1] = {0.95, 0.95, 0.95, 0.85, 0.85, 0.85, 0.85, 0.8
 
 //---------------------------------------------------------
 // BDT scores applied to produce the acceptance correction (chosen in order to have large purity)
-bool isProducedAcceptancePlots = 1; // 1 for acceptance production, 0 for default analysis
+bool isProducedAcceptancePlots = 0; // 1 for acceptance production, 0 for default analysis
 const float BDTscoreCutAcceptance[numCent + 1] = {0.96, 0.96, 0.96, 0.96, 0.96, 0.96, 0.96, 0.96, 0.96};
 //---------------------------------------------------------
 
@@ -208,11 +218,11 @@ const double BDTscoreCutPtIntLoosest[numCent + 1] = {0.96, 0.92, 0.88, 0.76, 0.5
 // BDT cut for integrated pt measurement, loosest cut that give a purity > 0.95 within Extrsigmacentral[1];
 
 // --------------------------- SYST ------------------------------
-const int trialsLambdaTopo = 200; // number of trials for the systematic studies related to Lambda topology
+const int trialsLambdaTopo = 396; // number of trials for the systematic studies related to Lambda topology
 // systematic studies on BDT score variation ----------------------
 bool ExtrisSysMultTrial = 0;       // 1 for systematic studies, 0 for default analysis
-bool ExtrisSysLambdaMultTrial = 1; // 1 for systematic studies, 0 for default analysis
-const int trialsBDT = 200;         // number of trials for the systematic studies related to BDTscore
+bool ExtrisSysLambdaMultTrial = 0; // 1 for systematic studies, 0 for default analysis
+const int trialsBDT = 201;         // number of trials for the systematic studies related to BDTscore
 const float nsigmaBarlow = 0;
 const float UpperlimitBDTscoreCut = 1;
 const float LowerlimitBDTscoreCut = 0.2;
@@ -288,5 +298,12 @@ Double_t V2OOPub[numV2OOPubCent] = {0.058265, 0.059672, 0.060794, 0.062044, 0.06
                                     0.067843, 0.068207, 0.068403, 0.068001, 0.067598, 0.066635, 0.066261, 0.064535};
 Double_t V2OOPubErrStat[numV2OOPubCent] = {0.000070, 0.000162, 0.000165, 0.000134, 0.000126, 0.000072, 0.000070, 0.000069,
                                            0.000090, 0.000095, 0.000123, 0.000084, 0.000165, 0.000186, 0.000198, 0.000196};
-Double_t V2OOPubErrSys[numV2OOPubCent] = {0.000226, 0.000232, 0.000236, 0.000241, 0.000241, 0.000248, 0.000254, 0.000260,
+Double_t V2OOPubErrSyst[numV2OOPubCent] = {0.000226, 0.000232, 0.000236, 0.000241, 0.000241, 0.000248, 0.000254, 0.000260,
                                           0.000263, 0.000265, 0.000266, 0.000264, 0.000263, 0.000259, 0.000257, 0.000251};
+
+//Published V2 of charged particles in PbPb collisions (arxiv.org/pdf/1602.01119)
+const Int_t numV2PbPbPubCent = 8;
+Double_t V2PbPbPubCent[numV2PbPbPubCent + 1] = {0, 10, 20, 30, 40, 50, 60, 70, 80};
+Double_t V2PbPbPub[numV2PbPbPubCent] = {(0.02839 + 0.04566)/2, 0.06551, 0.08707, 0.0991, 0.10414, 0.10286, 0.09746, 0.08881};
+Double_t V2PbPbPubErrStat[numV2PbPbPubCent] = {(0.00057 + 0.00064)/2, 0.00037, 0.00044, 0.00055, 0.00073, 0.00107, 0.00186, 0.00438};
+Double_t V2PbPbPubErrSys[numV2PbPbPubCent] = {(0.00043 + 0.00069)/2, 0.00098, 0.00131, 0.00149, 0.00156, 0.00154, 0.00146, 0.00133};
